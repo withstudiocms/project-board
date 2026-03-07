267367108cb8fb64802650ceb4721d3522ab243b
Adam Matthiesen <amatthiesen@outlook.com>
2026-03-07 03:29:08 -0800

    feat: enhance security configuration and refactor site URL definition in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+const primarySiteDomain = 'project-board-demo.studiocms.dev';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://project-board-demo.studiocms.dev',
+  site: `https://${primarySiteDomain}`,
   output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  security: {
+    allowedDomains: [
+      // Replace this value with your deployment provider's Environment variable for the deployment URL, if available for Preview deployments.
+      ...(process.env.DOKPLOY_DEPLOY_URL
+        ? [
+          {
+            hostname: process.env.DOKPLOY_DEPLOY_URL,
+            protocol: 'https:',
+          },
+        ]
+        : [
+          {
+            hostname: primarySiteDomain,
+            protocol: 'https:',
+          },
+          {
+            hostname: 'localhost',
+            protocol: 'http:',
+          },
+        ]),
+    ]
+  },
 });
